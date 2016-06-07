4d13421cde59732f432163acaa1a9fe3f9c523f8
Mouihena <spitan@163.com>
2016-06-07 21:08:01 +0800

    rdkit8

--- fileSearch/handleUploadFile.py
+++ fileSearch/handleUploadFile.py
@@ -5,7 +5,7 @@ def handleUploadFile(f):
     # with open('{% static "media/molecule.txt"%}', 'wb+') as destination:
     #     for chunk in f.chunks():
     #         destination.write(chunk)
-    mol = Chem.MolFromMolFile(f)
+    mol = Chem.MolFromMolFile(f,bool sanitize=True, bool removeHs=True, bool strictParsing=True)
     smiles = Chem.MolToSmiles(mol)
     return smiles
     # else:
