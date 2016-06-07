588b83d8badacdf53441cb29435018f9a7484747
Mouihena <spitan@163.com>
2016-06-08 01:13:02 +0800

    imago

--- fileSearch/handleUploadImage.py
+++ fileSearch/handleUploadImage.py
@@ -3,12 +3,12 @@ from rdkit import Chem
 
 def handleUploadImage(file_name):    
     # image to mol
-    command_cd = '/Users/apple/Documents/mysite/casfinder/media_cdn/uploads'
+    command_cd = '/home/ubuntu/myspaces/env1/mysite/casfinder/media_cdn/uploads'
     command_turn = './imago_console '+ file_name +' -o output.mol'
     os.system(command_cd)
     os.system(command_turn)
     
     # mol to smiles
-    m = Chem.MolFromMolFile('/Users/apple/Documents/mysite/casfinder/media_cdn/uploads/output.mol')
+    m = Chem.MolFromMolFile('/home/ubuntu/myspaces/env1/mysite/casfinder/media_cdn/uploads/output.mol')
     smiles = Chem.MolToSmiles(m)
     return smiles
