a6894e95d44d3672b88bd143d07c4ebf07c62ed6
Mouihena <spitan@163.com>
2016-06-07 22:12:44 +0800

    rdkit21

--- fileSearch/handleUploadFile.py
+++ fileSearch/handleUploadFile.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 from rdkit import Chem
 
 def handleUploadFile(f):    
-    with open('static/media/molecule.txt','w+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
-        # rdkit   
+    with open(f,'r') as destination:
+        # for chunk in f.chunks():
+        #    destination.write(chunk)
+            # rdkit   
         stringWithMolData=destination.read()
         m = Chem.MolFromMolBlock(stringWithMolData)
         smiles = Chem.MolToSmiles(m)
+        print(smiles)
     
     return smiles
     # else:
